33d636840fa4243d4c662d3dfd3dec8b97ca3091
Andre-s23 <160166204+Andre-s23@users.noreply.github.com>
2024-02-23 15:25:43 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,4 +2,18 @@
 
 int gcd(int a, int b) {
     // поместить сюда текст реализации алгоритма (на С++)
+    int max;
+    int count;
+    if (a > b) {
+        max = a;
+    }
+    else
+    {
+        max = b;
+    }
+    if (a == b) max = a;
+    for (int i = 1; i <= max; i++) {
+        if ((a % i == 0) && (b % i == 0)) count = i;
+    }
+    return count;
 }
